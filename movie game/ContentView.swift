bbe3ebab9756042f0c32edaa6c687b6c6a2e560e
Jacob Boyce <jboyce313@gmail.com>
2024-06-03 10:25:52 -0500

    Initial Commit

---
+++ movie game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie game
+//
+//  Created by Jacob Boyce on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie game/movie_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_gameApp.swift
+//  movie game
+//
+//  Created by Jacob Boyce on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct movie_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
